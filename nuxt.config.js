815656598c2b7a6e86819618883f56bf5bdb168f
wett <doll1988@yandex.ru>
2017-05-01 15:10:25 +0400

    init

---
+++ nuxt.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+  css: [
+    { src: '~assets/master.scss', lang: 'scss' },
+  ],
+
+  build: {
+    vendor: ['axios', 'lodash', 'moment', ],
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['> 5%']
+      })
+    ]
+  },
+
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js', body: true },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js', body: true },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/js/app.min.js', body: true },
+
+    ]
+  }
+
+}
